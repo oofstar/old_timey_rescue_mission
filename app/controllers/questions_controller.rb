abcd6583fd2681488c15c965acde3aea2c19b499
Al Fair <thealisonfair@gmail.com>
2017-08-28 15:20:11 -0400

    post-a-question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,5 +8,23 @@ class QuestionsController < ApplicationController
     @answers = Answer.where(question_id: params[:id])
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created.'
+    else
+      @errors = "#{@question.errors.full_messages}"
+      render :new
+    end
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
 
 end
